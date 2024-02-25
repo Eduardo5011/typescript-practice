b0764ac9be8a0423ddb22b3cebd1367392195885
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-02-25 18:25:48 -0500

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Message from './Message';
 
 function App() {
+  // let firstValue: string = "Manny";
+  // let firstValue: number = 34;
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [2, 3, 56];
+  // let firstValue: Array<string> = ['2', '3', '56'];
+
+  //tuple
+  // let aTuple: [string, number] = ["Manny", 34];
+  // // enum
+  // enum Codes {
+  //   first = 1,
+  //   second = 2,
+  // }
+  // //any
+  // let firstName: any = 3
+  // //void
+  // const warning = (): void => {
+  //   console.log('warning')
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message message='This is a simple message'/>
       </header>
     </div>
   );
